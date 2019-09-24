ac9b69d0d3a946be435f294416ed2dabc0275fd7
Montse <montse.dcm.311@gmail.com>
2019-09-24 10:33:06 -0500

    Delete some files

--- cypress/integration/Global-Navigation/Topbar-testcases.spec.js
+++ cypress/integration/Global-Navigation/Topbar-testcases.spec.js
@@ -3,17 +3,31 @@
 describe("Topbar tests",()=>{
 
 it("Verify the name of the app is displayed",()=>{
-    cy.viewport(1224,760);
-    cy.visit('http://localhost:4200');
-    cy.get('.navbar-start>span').contains('My Fleets');
+  cy.visit('');
+  cy.get('.navbar-start>span').contains('My Fleets');
   
 })
 
 it("Verify the links are displayed",()=>{
-    cy.viewport(1224,760);
-    cy.visit('http://localhost:4200');
-    
+ cy.visit('');
+ cy.get('a.navbar-item.is-link:nth-of-type(1)').should('contain','Dashboard');
+ cy.get('a.navbar-item.is-link:nth-of-type(2)').should('contain','Resources');
+ cy.get('a.navbar-item.is-link:nth-of-type(3)').should('contain','People');
+ cy.get('a.navbar-item.is-link:nth-of-type(4)').should('contain','Places');
+ cy.get('a.navbar-item.is-link:nth-of-type(5)').should('contain','Groups');
 
-})
+ cy.get('#notificationBtn');
+ cy.get('#helpBtn');
+ })
 
+ it ("Verify Links are clickable and user is directed to the expected Url",()=>{
+    cy.visit('');
+    cy.get('a.navbar-item.is-link:nth-of-type(1)').click().url().should('include','/dashboard');
+    cy.get('a.navbar-item.is-link:nth-of-type(2)').click().url().should('include','/resources');
+    cy.get('a.navbar-item.is-link:nth-of-type(3)').click().url().should('include','/people');
+    cy.get('a.navbar-item.is-link:nth-of-type(4)').click().url().should('include','/places');
+    cy.get('a.navbar-item.is-link:nth-of-type(5)').click().url().should('include','/groups');
+    cy.get('#notificationBtn').click();
+    cy.get('#helpBtn').click();
+ })
 })
